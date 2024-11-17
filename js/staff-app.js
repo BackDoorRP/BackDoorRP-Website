ffe20963aaf51b54a760628fb0aacf2627a9ae68
BackDoorRP <backdoorrp@protonmail.com>
2024-11-17 14:18:02 -0800

    updated google app scripts

--- js/staff-app.js
+++ js/staff-app.js
@@ -4,7 +4,7 @@ form.addEventListener("submit", async function(event) {
         const formData = new FormData(form);
         const data = Object.fromEntries(formData);
         try {
-                const response = await fetch("https://script.google.com/macros/s/AKfycbxK5sLxNXdBjB-oJKCWFAJPN4DYMMglzGoEVIGa9_AMVY50FDDPblrPgDtEXvFFnwYA/exec", {
+                const response = await fetch("https://script.google.com/macros/s/AKfycbzly5-jGiGOBhmBwidX-sm9_gsuMo7m_O_SEgxmMMuSrl0wVGxOZwexZLRSKvVci9Cp/exec", {
                         method: "POST",
                         body: new URLSearchParams(data)
                 });
